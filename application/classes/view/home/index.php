bca4e077166275ecf7a006cccae4243ee930c9a7
tdroL <stdrol@gmail.com>
2012-01-09 22:08:50 +0100

    Added random string generator (cookie salt) to home page

--- application/classes/view/home/index.php
+++ application/classes/view/home/index.php
@@ -2,6 +2,11 @@
 
 class View_Home_Index extends View_Layout {
 
+	public function random()
+	{
+		return str_replace('\'', '\\\'', Text::random('qwertyuiopasdfghjklzxcvbnm1234567890!@#$%^&*()-=_+[]{};\'\|,.<>/?:"', 64));
+	}
+
 	public function as_json()
 	{
 		return array();
