5b746470b5911a8f2a78a6999565c983a5749ba0
tdroL <stdrol@gmail.com>
2012-02-21 12:57:29 +0100

    Changed database drivers names to follow PSR-0

--- index.php
+++ index.php
@@ -114,7 +114,7 @@ else
 	* Execute the main request. A source of the URI can be passed, eg: $_SERVER['PATH_INFO'].
 	* If no source is specified, the URI will be automatically detected.
 	*/
-	echo Request::factory()
+	echo Request::factory(TRUE, array(), FALSE)
 		->execute()
 		->send_headers()
 		->body();
