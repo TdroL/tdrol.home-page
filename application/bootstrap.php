0a2b3e6573358789f2946ced8f125cf4559cb447
TdroL <stdrol@gmail.com>
2012-02-23 15:30:25 +0100

    Switched to Jelly 3.3/develop

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -104,7 +104,7 @@ Kohana::modules(array(
 	'cache'           => MODPATH.'cache',           // Cache
 	'database-sqlite' => MODPATH.'database-sqlite', // SQLite driver
 	'database'        => MODPATH.'database',        // Database access
-	'orm'             => MODPATH.'orm',             // ORM
+	'jelly'           => MODPATH.'jelly',           // Jelly
 	'kostache'        => MODPATH.'kostache',        // KOstache
 	'yaminify'        => MODPATH.'yaminify',        // Yaminify
 	'yaminify-assets' => MODPATH.'yaminify-assets', // Yaminify - Asset bridge
